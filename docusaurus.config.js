003c7bdd03df4f8a34977ac7f1a4213dbea611fa
luxcgo <luxcgo@gmail.com>
2023-06-24 00:19:00 +0800

    seo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -192,7 +192,8 @@ const config = {
       metadata: [
         {
           name: "keywords",
-          content: "live stream recorder, cams recorder",
+          content:
+            "live stream recorder, cams recorder, afreecatv live stream record bigo live stream record bilibili live stream record cc live stream record douyin live stream record douyu live stream record huya live stream record nimo live stream record pixiv live stream record tiktok live stream record twitch live stream record youtube live stream record yy live stream record bongacams live stream record cam4 live stream record chaturbate live stream record stripchat live stream record",
         },
       ],
     }),
