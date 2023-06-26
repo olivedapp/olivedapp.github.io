35d2dc9ed03d84dc2dedd599a649de6516742027
luxcgo <luxcgo@gmail.com>
2023-06-26 10:51:38 +0800

    fix canonical url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
-  url: "https://olivedapp.github.io",
+  url: "https://olived.app",
   baseUrl: "/",
   organizationName: "olivedapp",
   projectName: "olivedapp.github.io",
